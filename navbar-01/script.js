94ae7309fa291fbe07a327bb12799964b1bdfffc
abanoj <jaabanof@gmail.com>
2023-04-22 20:26:56 +0200

    Some changes

--- navbar-01/script.js
+++ navbar-01/script.js
@@ -1,11 +1,14 @@
 const d = document;
 
 //Ejecutar al cargar el documento
-d.addEventListener("DOMContentLoaded", (e) => {
-});
+d.addEventListener("DOMContentLoaded", (e) => {});
 
-d.addEventListener('click', e => {
-  if(e.target.matches('.menu a') || e.target.matches('.navbar-burger')){
-    d.querySelector('body').classList.toggle('change');
+d.addEventListener("click", (e) => {
+  if (
+    e.target.matches(".menu-2 a") ||
+    e.target.matches(".navbar-burger") ||
+    e.target.matches(".navbar-burger *")
+  ) {
+    d.querySelector("body").classList.toggle("active");
   }
-})
+});
